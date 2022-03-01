1aada22b822bd0483ea4dedcf8bf9d3aab50e43e
sgroberg <sgroberg@gmail.com>
2022-03-01 11:38:55 -0500

    first commit with update to UserScript

---
+++ UserScript.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         5mu3: {WHAT}
+// @namespace    mailto:sgroberg@gmail.com
+// @version      1.1
+// @description  {What Do}
+// @author       5mu3_
+// @match        https://townstar.sandbox-games.com/launch
+// @grant        none
+// @run-at       document-start
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// ==/UserScript==
